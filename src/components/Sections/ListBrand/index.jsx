91c283662a5054bd9a803cf436b5a41b7ddeef30
daoanhhuy26012001 <daoanhhuy26012001@gmail.com>
2023-06-09 12:11:56 +0700

    AA

--- src/components/Sections/ListBrand/index.jsx
+++ src/components/Sections/ListBrand/index.jsx
@@ -2,12 +2,14 @@ import './ListBrand.css';
 import { Button } from '../../Common/Button';
 import { VARIANTS } from '../../../constants/constant';
 
+import image from '../../../assets/images/down.png'
+
 export const ListBrand = ({ brand }) => {
   return (
     <ul className="list-brand">
       {brand.map((url, index) => (
         <li key={index}>
-          <Button variant={VARIANTS.SECONDARY} url={ 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRENI8qnl4Ag4V7c-FHNuVSCO4IPc5pJ29bLeXx-ATSWiP_CrgZAGY8KexqXUgkpT87lGM'} />
+          <Button variant={VARIANTS.SECONDARY} url={image} />
         </li>
       ))}
     </ul>
